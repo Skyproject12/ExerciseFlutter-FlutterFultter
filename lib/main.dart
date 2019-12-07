3c64ea84129d26684fb299e5d575bdfd30bd4664
sumendra12 <sumendrakadek29@gmail.com>
2019-12-07 23:08:32 +0700

    cutom drawer

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
-import'package:flutter/material.dart';
-void main(){
-  runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home:HomePage(),
-    )
-  );
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomePage(),
+  ));
 }
+
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
@@ -20,13 +20,35 @@ class _HomePageState extends State<HomePage> {
       // melakukan custom in toolbar
       appBar: new AppBar(
         // change background color
-        backgroundColor:Colors.red,
+        backgroundColor: Colors.red,
         title: Text('ShopApp'),
         actions: <Widget>[
           // add icon search in toolbar
-          new IconButton(icon: Icon(Icons.search, color: Colors.white), onPressed: (){}),
+          new IconButton(
+              icon: Icon(Icons.search, color: Colors.white), onPressed: () {}),
+          new IconButton(
+              icon: Icon(Icons.shopping_cart, color: Colors.white),
+              onPressed: () {}),
           // mengeset color icon menjadi white
         ],
+      ),
+      drawer: new Drawer(
+        // make simple drawer
+        child: new ListView(
+          children: <Widget>[
+            // that can make header for drawer
+            new UserAccountsDrawerHeader(
+                accountName: Text('Santos Enoque'),
+                accountEmail: Text('santosenoque@gmail.com'),
+            currentAccountPicture:GestureDetector(
+              // make circle avatar
+              child:new CircleAvatar(
+                backgroundColor: Colors.grey,
+                child:Icon(Icons.person, color:Colors.white)
+              )
+            ) ,),
+          ],
+        ),
       ),
     );
   }
