1f1e3d30c7e19caf6727bd0a75aded86cd004cdd
sumendra12 <sumendrakadek29@gmail.com>
2019-12-07 22:40:56 +0700

    make custom toolbar

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import'package:flutter/material.dart';
+void main(){
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home:HomePage(),
+    )
+  );
+}
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    // merupakan suatu layout yang mengatur design baik itu appbars, drawers, snackbars
+    return Scaffold(
+      // melakukan custom in toolbar
+      appBar: new AppBar(
+        // change background color
+        backgroundColor:Colors.red,
+        title: Text('ShopApp'),
+        actions: <Widget>[
+          // add icon search in toolbar
+          new IconButton(icon: Icon(Icons.search, color: Colors.white), onPressed: (){}),
+          // mengeset color icon menjadi white
+        ],
+      ),
+    );
+  }
+}
