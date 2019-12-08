97f9c4f52cd0f2ec1bbdca80607b11cc933cd103
sumendra12 <sumendrakadek29@gmail.com>
2019-12-08 09:52:09 +0700

    fix body of drawer menu

--- lib/main.dart
+++ lib/main.dart
@@ -38,15 +38,87 @@ class _HomePageState extends State<HomePage> {
           children: <Widget>[
             // that can make header for drawer
             new UserAccountsDrawerHeader(
-                accountName: Text('Santos Enoque'),
-                accountEmail: Text('santosenoque@gmail.com'),
-            currentAccountPicture:GestureDetector(
-              // make circle avatar
-              child:new CircleAvatar(
-                backgroundColor: Colors.grey,
-                child:Icon(Icons.person, color:Colors.white)
-              )
-            ) ,),
+              accountName: Text('Santos Enoque'),
+              accountEmail: Text('santosenoque@gmail.com'),
+              currentAccountPicture: GestureDetector(
+                  // make circle avatar
+                  child: new CircleAvatar(
+                      backgroundColor: Colors.grey,
+                      child: Icon(Icons.person, color: Colors.white))),
+              decoration: new BoxDecoration(
+                  // change box decoration color is red
+                  color: Colors.red),
+            ),
+
+            //body the drawer
+            InkWell(
+              // make onclick
+              onTap: () {},
+              child: ListTile(
+                // make isi  in drawer
+                title: Text('Home Page'),
+                leading: Icon(Icons.home),
+              ),
+            ),
+            InkWell(
+              // make onclick
+              onTap: () {},
+              child: ListTile(
+                // make isi  in drawer
+                title: Text('My Account'),
+                leading: Icon(Icons.person),
+              ),
+            ),
+            InkWell(
+              // make onclick
+              onTap: () {},
+              child: ListTile(
+                // make isi  in drawer
+                title: Text('My orders'),
+                leading: Icon(Icons.shopping_basket),
+              ),
+            ),
+            InkWell(
+              // make onclick
+              onTap: () {},
+              child: ListTile(
+                // make isi  in drawer
+                title: Text('Category'),
+                leading: Icon(Icons.dashboard),
+              ),
+            ),
+            InkWell(
+              // make onclick
+              onTap: (){},
+              child: ListTile(
+                // make isi  in drawer
+                title: Text('Favorite'),
+                leading: Icon(Icons.favorite),
+
+              ),
+            ),
+            // memberi batas kepada suatu menu drawer
+            Divider(),
+            InkWell(
+              // make onclick
+              onTap: (){},
+              child: ListTile(
+                // make isi  in drawer
+                title: Text('Settings'),
+                leading: Icon(Icons.settings),
+
+              ),
+            ),
+            InkWell(
+              // make onclick
+              onTap: (){},
+              child: ListTile(
+                // make isi  in drawer
+                title: Text('About'),
+                leading: Icon(Icons.help),
+
+              ),
+            ),
           ],
         ),
       ),
